e7fd818aebe16392683360868a5fb07be6f895c9
DBFocus <shadow_wu82@163.com>
2012-11-23 17:32:09 +0800

    finish register function and update index function

--- Lib/Action/UserAction.class.php
+++ Lib/Action/UserAction.class.php
@@ -11,7 +11,37 @@ class UserAction extends Action {
 	
 	public function addUser() {
 		if ($this->isPost()) {
+			$User = D('User');
+			if ($User->create()){
+				if(!preg_match('/^.{6,12}$/', $User->password)){
+					$this->error('密码有效长度为6~12位');
+				}				
+				$User->password = sha1($User->password);
+				if($user_id = $User->add()) {
+					$this->success('您的申请已提交');
+				} else {
+					$this->error('申请提交失败');
+				}					
+			} else {
+				$this->error($User->getError());
+			}
+		}
+	}
+	
+	public function index() {
+		if ($this->isPost()) {
+			$email = $_POST['email'];
+			$password = sha1($_POST['password']);
 			
+			$User->M('User');
+			if ($currUser = $User->where("username = '$username' and password = '$password' and is_active = 1")
+								->field("user_id, username, is_admin")
+								->find()) {
+				setSessionCookie($currUser['user_id'], $currUser['username'], 1);
+				$this->success();	
+			} else {
+				$this->error('Email或密码有误');
+			}
 		}
 	}
 }
